dc018a94100ac89cde5931ae751d449556c11780
Didarul Karim Jewel <m.diarulkarimjewel@gmail.com>
2019-09-08 21:20:25 +0600

    registraion page customized

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,97 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 3 | Registration Page</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('/')}}assets/dist/css/adminlte.min.css">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+</head>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body class="hold-transition register-page">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+<div class="register-box" style="margin-top: 40px">
+    <div class="card">
+        <div class="card-body register-card-body">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+            <p class="login-box-msg">Register as New Member</p>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" name="name" value="{{ old('name') }}"
+                           placeholder="Your Name">
+                    <span class="text-danger"> {{$errors->has('name') ? $errors->first('name'):''}} </span>
                 </div>
+
+                <div class="form-group has-feedback">
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}"
+                           placeholder="Email">
+                    <span class="text-danger"> {{$errors->has('email') ? $errors->first('email'):''}} </span>
+
+                </div>
+
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    <span class="text-danger"> {{$errors->has('password') ? $errors->first('password'):''}} </span>
+
+                </div>
+
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" name="password_confirmation"
+                           placeholder="Retype password">
+                    <span
+                        class="text-danger"> {{$errors->has('password_confirmation') ? $errors->first('password_confirmation'):''}} </span>
+
+                </div>
+
+                <div>
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+                </div>
+
+            </form>
+
+            <div class="social-auth-links text-center">
+
+                <p>- OR -</p>
+
+                <a href="#" class="btn btn-block btn-primary">
+                    <i class="fa fa-facebook mr-2"></i>
+                    Sign up using Facebook
+                </a>
+
+                <a href="#" class="btn btn-block btn-danger">
+                    <i class="fa fa-google-plus mr-2"></i>
+                    Sign up using Google+
+                </a>
             </div>
+
+            <a href="{{route('login')}}" style="margin-left: 50px">I already have a membership</a>
         </div>
+
     </div>
+
 </div>
-@endsection
+
+
+<!-- jQuery -->
+<script src="{{asset('/')}}assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/')}}assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
