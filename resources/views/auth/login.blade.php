7def63dd0f95c40d42a7096cf3292624fa48e41d
Didarul Karim Jewel <m.diarulkarimjewel@gmail.com>
2019-09-08 21:19:01 +0600

    login page customized and required assets added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,116 @@
-@extends('layouts.app')
+<!DOCTYPE html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<html>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <title>Login</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('/')}}assets/dist/css/adminlte.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{asset('/')}}assets/plugins/iCheck/square/blue.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+</head>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+<body class="hold-transition login-page">
+<div class="login-box" style="margin-top: 50px">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">Sign In to Start Your Journey</p>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+                <div class="form-group has-feedback">
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+                    <span class="text-danger"> {{$errors->has('email') ? $errors->first('email'):''}} </span>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
                 </div>
+
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" name="password" required autocomplete="current-password" placeholder="Password">
+                    <span class="text-danger"> {{$errors->has('password') ? $errors->first('password'):''}} </span>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-8">
+                        <div class="checkbox icheck">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                            <label class="form-check-label" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
+                        </div>
+                    </div>
+
+
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+
+                </div>
+
+            </form>
+
+            <div class="social-auth-links text-center mb-3">
+
+                <p>- OR -</p>
+
+                <!--Social-Auth-Links -->
+                <a href="" class="btn btn-block btn-primary">
+                    <i class="fa fa-facebook mr-2"></i> Sign in using Facebook
+                </a>
+
+                <a href="" class="btn btn-block btn-danger">
+                    <i class="fa fa-google-plus mr-2"></i> Sign in using Google+
+                </a>
+
             </div>
+
+            @if (Route::has('password.request'))
+                <p class="mb-1">
+                    <a href="{{ route('password.request') }}" style="margin-left: 88px">I forgot my password</a>
+                </p>
+            @endif
+
+            <p class="mb-0">
+                <a href="{{route('register')}}" style="margin-left: 65px">Register a new membership</a>
+            </p>
         </div>
+
     </div>
 </div>
-@endsection
+
+
+<!-- jQuery -->
+<script src="{{asset('/')}}assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/')}}assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- iCheck -->
+<script src="{{asset('/')}}assets/plugins/iCheck/icheck.min.js"></script>
+<script>
+    $(function () {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        })
+    })
+</script>
+</body>
+</html>
